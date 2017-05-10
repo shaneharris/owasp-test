ee0af8ea1d60e9a8ebdf15e7b56741e88d431593
shaneharris <shane@xactaccounts.co.uk>
2017-05-10 12:07:20 +0100

    ajax request with json

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by autoc on 10/05/2017.
+ */
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var fs = require('fs');
+app.use(express.static(__dirname + '/web-root-root/'));
+app.get('/', function(req, res,next) {
+	res.sendFile(__dirname + '/web-root-root/index.html');
+});
+app.get('/api',function(req,res){
+	res.json({hello:'world'});
+})
+server.listen(8081);
